2dcca5df3f3b7097d25479b3a6e8e3706311f7b3
squamuglia <maxsmouha@gmail.com>
2020-01-20 15:35:54 -0800

    typesripted

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,51 @@
 import React, { useState } from "react";
 import About from "../components/about";
 import Home from "../components/home";
-import "../styles/index.css";
 import Head from "next/head";
+import "../styles/index.css";
+import { NextPage } from "next";
 
-export default () => {
-  const [fade, changeDisplayAbout] = useState("none");
-  const [displayHome, toggleHome] = useState(true);
-  const [backgroundColor, changeColor] = useState({ backgroundColor: "blue" });
+const App: NextPage = () => {
+  const [fade, setFade] = useState<"fadeIn" | "fadeOut" | "none">("none");
+  const [displayHome, setDisplayHome] = useState<boolean>(true);
+  const [backgroundColor, setColor] = useState<{ backgroundColor: string }>({
+    backgroundColor: "blue"
+  });
 
   const hoverColor = e => {
-    const x = window.innerWidth;
-    const y = window.innerHeight;
-    const mX = e.clientX;
-    const mY = e.clientY;
+    const x: number = window.innerWidth;
+    const y: number = window.innerHeight;
+    const mX: number = e.clientX;
+    const mY: number = e.clientY;
 
-    // console.log("mouse x", mX / x, "mouse y", mY / y);
-
-    changeColor({
+    setColor({
       backgroundColor: `linear-gradient(130deg, rgb(${(200 * mX) / x},${(200 *
         mY) /
         y},200), rgb(${(200 * mY) / y},120,${(200 * x) / mX})), 
-			linear-gradient(210deg, rgb(${(200 * mX) / x},${(200 * mY) / y},${(200 * mX) /
+        linear-gradient(210deg, rgb(${(200 * mX) / x},${(200 * mY) / y},${(200 *
+        mX) /
         x}), rgb(160,${(200 * mY) / y},${(200 * mX) / x})),
-			linear-gradient(330deg, rgb(80,${(200 * y) / mY},${(200 * mX) /
+          linear-gradient(330deg, rgb(80,${(200 * y) / mY},${(200 * mX) /
         x}), rgb(100,${(200 * mY) / y},${(200 * mX) / x}))`
     });
 
-    console.log(backgroundColor);
+    console.log("mouse x", mX / x, "mouse y", mY / y);
   };
 
-  const toggleAbout = () => {
-    if (displayAbout === "fadeIn") {
-      changeDisplayAbout("fadeOut");
-      toggleHome(false);
-      return setTimeout(() => changeDisplayAbout("none"), 500);
+  const toggleHome = () => {
+    if (fade === "fadeIn") {
+      setFade("fadeOut");
+      setDisplayHome(false);
+      return setTimeout(() => setFade("none"), 500);
     } else {
-      changeDisplayAbout("fadeIn");
-      toggleHome(true);
+      setFade("fadeIn");
+      setDisplayHome(true);
     }
   };
 
   return (
     <>
       <Head>
-        <meta charset="utf-8" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
@@ -52,7 +53,7 @@ export default () => {
         <meta name="theme-color" content="#000000" />
         <meta
           name="description"
-          content="I build friendly things in React, Node, WordPress & Shopify"
+          content="I build friendly things in React, Node, WordPress &amp; Shopify"
         />
         <meta
           name="keywords"
@@ -73,18 +74,23 @@ export default () => {
 
         <div
           className="top right abs my1 mx2 pt05 white o-4 z10"
-          onClick={toggleAbout}
+          onClick={toggleHome}
         >
-          {displayAbout === "none" ? "About" : "X"}
+          {fade === "none" ? "About" : "X"}
         </div>
 
         <div className="aic jcc f fw p1 yview">
           <div className="gutter px2 fa ac o-4 white">
-            <About style={displayAbout} toggle={toggleAbout} />
-            <Home style={displayHome ? { display: "none" } : null} />
+            {displayHome ? (
+              <Home />
+            ) : (
+              <About style={fade} toggle={toggleHome} />
+            )}
           </div>
         </div>
       </div>
     </>
   );
 };
+
+export default App;
