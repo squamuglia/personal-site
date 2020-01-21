e01f06b935556787f8172d495e93755125e9e6e0
squamuglia <maxsmouha@gmail.com>
2020-01-21 08:48:58 -0800

    updated bio

--- components/about.tsx
+++ components/about.tsx
@@ -15,11 +15,13 @@ const About: NextPage<Props> = ({ toggle, style }) => (
       <div className="round mxa mb1 __me" />
       <h3>Hi, it's me.</h3>
       <p>
-        I'm a full-stack design developer based in Brooklyn. I build friendly
-        things in React, Node, WordPress {"&"} Shopify.
+        I'm a front-end developer based in Brooklyn with an eye for design and
+        product. I build friendly things in React, Node, Shopify, {"&"} more.
       </p>
       <p>
-        I like fun projects and people. If you'd like to collaborate,{" "}
+        I like fun projects and people, especially when they exploit suprirising
+        sensory phenomena. My interests include VR, Video Chat, SMS and
+        experiential design. If you'd like to collaborate,{" "}
         <a href="mailto:maxsmouha@gmail.com">email me</a>, or just feel a sense
         of satisfaction from knowing you could contact me at any time.
       </p>
