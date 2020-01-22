c3e5991c54a7b45a875377d00bcbbc5ace4e6b33
squamuglia <maxsmouha@gmail.com>
2020-01-21 17:05:35 -0800

    all korrect

--- components/fader.tsx
+++ components/fader.tsx
@@ -4,14 +4,14 @@ import { NextPage } from "next";
 const headlines = [
   "who is afraid of horses.",
   "who pod races.",
-  "who isn't afraid to say we met on Tinder."
-  // "who so random.",
-  // "who finagles.",
-  // "who does the madison.",
-  // "who understands eggs.",
-  // "who loves the word 'externality'.",
-  // "who knows a guy.",
-  // "who can even."
+  "who isn't afraid to say we met on Tinder.",
+  "who so random.",
+  "who finagles.",
+  "who does the madison.",
+  "who understands eggs.",
+  "who loves the word 'externality'.",
+  "who knows a guy.",
+  "who can even."
 ];
 
 const Fader: NextPage = () => {
@@ -19,17 +19,20 @@ const Fader: NextPage = () => {
   const [headlineIdx, setHeadline] = useState<number>(0);
 
   useEffect(() => {
-    //adding this style on component mount seems to fix syncing issue
+    // adding this style on component mount fixes syncing issue
     if (fadeRef) {
       fadeRef.classList = "loop-fade";
     }
 
-    //randomize headlines on component mount
+    // randomize headlines on component mount
     headlines.sort(() => Math.round(Math.random()));
 
-    //setInterval has a closed scope and doesn't have access to the state!
-    //we don't want to mutate the state, however
+    /* setInterval has a closed scope and doesn't have access to the state, 
+    hence `let i`, but we still need to update state to refresh the headline 
+    setting the state right here also prevents a funny bug where the first headline repeats twice */
     let i = headlineIdx;
+    setHeadline(i++);
+
     const time = setInterval(() => {
       if (i === headlines.length) {
         i = 0;
