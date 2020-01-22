0e2fb5c1f5357cfb51eebbf3b10b69b411516563
squamuglia <maxsmouha@gmail.com>
2020-01-21 16:55:40 -0800

    fixed fader

--- components/fader.tsx
+++ components/fader.tsx
@@ -4,36 +4,42 @@ import { NextPage } from "next";
 const headlines = [
   "who is afraid of horses.",
   "who pod races.",
-  `who isn't afraid to say we met on Tinder.`,
-  "who so random.",
-  "who finagles.",
-  "who does the madison.",
-  "who understands eggs.",
-  "who loves the word 'externality'.",
-  "who knows a guy.",
-  "who can even."
-].sort(() => Math.round(Math.random()));
+  "who isn't afraid to say we met on Tinder."
+  // "who so random.",
+  // "who finagles.",
+  // "who does the madison.",
+  // "who understands eggs.",
+  // "who loves the word 'externality'.",
+  // "who knows a guy.",
+  // "who can even."
+];
 
 const Fader: NextPage = () => {
   let fadeRef;
-  let [headlineIdx, setHeadline] = useState<number>(0);
+  const [headlineIdx, setHeadline] = useState<number>(0);
 
   useEffect(() => {
+    //adding this style on component mount seems to fix syncing issue
     if (fadeRef) {
-      //adding this style on component mount seems to fix syncing issue
       fadeRef.classList = "loop-fade";
     }
 
+    //randomize headlines on component mount
+    headlines.sort(() => Math.round(Math.random()));
+
+    //setInterval has a closed scope and doesn't have access to the state!
+    //we don't want to mutate the state, however
+    let i = headlineIdx;
     const time = setInterval(() => {
-      headlineIdx === headlines.length - 1
-        ? setHeadline(0)
-        : setHeadline(headlineIdx++);
+      if (i === headlines.length) {
+        i = 0;
+      }
+      setHeadline(i++);
     }, 3000);
 
     return () => clearInterval(time);
   }, []);
 
-  console.log(headlines);
   return (
     <span className="none" ref={node => (fadeRef = node)}>
       {headlines[headlineIdx]}
@@ -42,8 +48,3 @@ const Fader: NextPage = () => {
 };
 
 export default Fader;
-
-// console.log("hi: ", headlineIdx);
-// console.log(headlineIdx === headlines.length);
-// console.log("update: ", headlineIdx + 1);
-// console.log("current: ", headlines[headlineIdx]);
